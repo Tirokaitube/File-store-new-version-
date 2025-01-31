012c95879f75401183afca6775d8401541950ce7
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2025-01-31 13:20:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -9,13 +9,7 @@ class script(object):
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
     
-    CAPTION = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_name}
-
-sɪᴢᴇ ⚙️: {file_size}
-
-Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/Ayu_bots)</b>""" 
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
